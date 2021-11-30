b63dbf947e5ae49c7fd2539231d4c5dd27745f20
chinguy <chinguy@metropolia.fi>
2021-11-30 09:31:31 +0200

    first commit, empty project

---
+++ app/src/main/java/fi/chinguyen/betterskin/MainActivity.java
@@ -0,0 +1,14 @@
+package fi.chinguyen.betterskin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
