a272a04ff54dc0c63628de266fc1205d4abd5400
chinguy <chinguy@metropolia.fi>
2021-12-04 21:24:13 +0200

    change main to logo display

--- app/src/main/java/fi/chinguyen/betterskin/MainActivity.java
+++ app/src/main/java/fi/chinguyen/betterskin/MainActivity.java
@@ -10,21 +10,13 @@ import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    Timer timer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, Welcome.class);
-                startActivity(intent);
-                //finish();
-            }
-        },3000);
+
     }
 }
