289f8f19468edae3df07e90568a33e3afb4cd7ef
chinguy <chinguy@metropolia.fi>
2021-12-17 17:09:35 +0200

    fix bug with quiz 2

--- app/src/main/java/fi/chinguyen/betterskin/SecondQuiz.java
+++ app/src/main/java/fi/chinguyen/betterskin/SecondQuiz.java
@@ -5,27 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class SecondQuiz extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.better-skin.MESSAGE";
     private String userSkinGoal;
+    RadioGroup mainGoalGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_quiz);
+
+        mainGoalGroup= findViewById(R.id.mainGoalGroup);
+
+        Button goToThirdQuiz = findViewById(R.id.btnNext2);
+        //Go to third quiz when button "next" is clicked
+        goToThirdQuiz.setOnClickListener(view ->{
+            //Set condition for user to choose one choice
+            if(mainGoalGroup.getCheckedRadioButtonId() == -1){
+                //If user choose nothing, show a message
+                Toast.makeText(SecondQuiz.this,"Please choose one",Toast.LENGTH_SHORT).show();
+            }else{
+                //If user chose one, save user choice and go to third Quiz
+                userQuizChoices.getInstance().addToUserChoices(userSkinGoal);
+                Intent intent = new Intent(this, ThirdQuiz.class);
+                intent.putExtra(EXTRA_MESSAGE, userSkinGoal);
+                startActivity(intent);
+            }
+        });
+
     }
 
     //Method to do while radio button in second quiz is clicked
     public void getSkinMainGoal(View view){
-        RadioGroup mainGoalGroup= findViewById(R.id.mainGoalGroup);
         // Listen to the user's choice
-        RadioButton skinMainGoal  = (RadioButton)findViewById(mainGoalGroup.getCheckedRadioButtonId());
+        RadioButton skinMainGoal  = findViewById(mainGoalGroup.getCheckedRadioButtonId());
         // Get RadioButton text
         userSkinGoal = skinMainGoal.getText().toString();
     }
-
+/*
     //Go to third quiz when button "next" is clicked
     public void goToThirdQuiz(View view) {
         //Add user's choice to singleton arrayList
@@ -33,5 +54,5 @@ public class SecondQuiz extends AppCompatActivity {
         Intent intent = new Intent(this, ThirdQuiz.class);
         intent.putExtra(EXTRA_MESSAGE, userSkinGoal);
         startActivity(intent);
-    }
+    }*/
 }
