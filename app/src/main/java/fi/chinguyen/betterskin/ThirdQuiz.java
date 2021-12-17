d488644a900a03d205ad61b82122245a5719788e
chinguy <chinguy@metropolia.fi>
2021-12-17 17:25:01 +0200

    fix bug with quiz3

--- app/src/main/java/fi/chinguyen/betterskin/ThirdQuiz.java
+++ app/src/main/java/fi/chinguyen/betterskin/ThirdQuiz.java
@@ -5,33 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class ThirdQuiz extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.better-skin.MESSAGE";
     private String userSensitivity;
+    public  RadioGroup sensitiveSkin;
+    public RadioButton selectedSensitiveSkin;
+    public Button goToGenerateRoutine;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_quiz);
+
+        sensitiveSkin = findViewById(R.id.sensitiveRadioGroup);
+        goToGenerateRoutine = findViewById(R.id.btnNext3);
+
+        //Set a condition when user click Generate Routine button
+        goToGenerateRoutine.setOnClickListener(view->{
+            if(sensitiveSkin.getCheckedRadioButtonId() == -1){
+                //If user choose nothing, show a message
+                Toast.makeText(ThirdQuiz.this,"Please choose one!",Toast.LENGTH_SHORT).show();
+            }else{
+                //If user choose one, go to next activity
+                //Add user's choice to singleton arrayList
+                userQuizChoices.getInstance().addToUserChoices(userSensitivity);
+                Intent intent = new Intent(this, GenerateMorningRoutine.class);
+                intent.putExtra(EXTRA_MESSAGE, userSensitivity);
+                startActivity(intent);
+            }
+        });
+
     }
 
     //Method to do while radio button in third quiz is clicked
     public void getSkinSensitivity(View view){
-        RadioGroup sensitiveSkin = findViewById(R.id.sensitiveRadioGroup);
         // Listen to the user's choice
-        RadioButton selectedSensitiveSkin  = findViewById(sensitiveSkin.getCheckedRadioButtonId());
+        selectedSensitiveSkin  = findViewById(sensitiveSkin.getCheckedRadioButtonId());
         // Get RadioButton text
         userSensitivity = selectedSensitiveSkin.getText().toString();
     }
 
-    //Go to GenerateMorningRoutine when button is clicked
+   /* //Go to GenerateMorningRoutine when button is clicked
     public void goToGenerateRoutine(View view) {
         //Add user's choice to singleton arrayList
         userQuizChoices.getInstance().addToUserChoices(userSensitivity);
         Intent intent = new Intent(this, GenerateMorningRoutine.class);
         intent.putExtra(EXTRA_MESSAGE, userSensitivity);
         startActivity(intent);
-    }
+    }*/
 }
